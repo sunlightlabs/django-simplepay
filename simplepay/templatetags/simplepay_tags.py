d9dbb73f27823799ce91bd56ed2b50b31360569e
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-10-12 14:33:50 -0400

    add _coerce_form method

--- simplepay/templatetags/simplepay_tags.py
+++ simplepay/templatetags/simplepay_tags.py
@@ -1,7 +1,8 @@
 from django import template
+from django.forms import Form
 from django.template.loader import render_to_string
 from simplepay import HOST, PATH
-from simplepay.forms import PaymentForm, DonationForm
+from simplepay.forms import SimplePayForm, PaymentForm, DonationForm
 
 register = template.Library()
 
@@ -16,6 +17,11 @@ def payment_form(form):
     if not isinstance(form, PaymentForm):
         raise ValueError('PaymentForm is required')
     return _render_form(form, 'simplepay/form_payment.html')
+
+def _coerce_form(form_or_id):
+    if isinstance(form_or_id, Form):
+        return form_or_id
+    return SimplePayForm.objects.get(pk=form_or_id).get_real_obj()
 
 def _render_form(form, template):
     return render_to_string(template, {
