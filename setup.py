1ad3c1e4921fe2ca5fb343307041484286e4dc7b
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-10-14 17:08:07 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name='django-simplepay',
+    version="0.1",
+    packages=['simplepay'],
+    package_dir={'simplepay': 'simplepay'},
+    package_data={'simplepay': ['templates/simplepay/*.html']},
+    description='Amazon Simple Pay buttons',
+    author='Jeremy Carbaugh',
+    author_email='jcarbaugh@sunlightfoundation.com',
+    license='BSD License',
+    url='http://github.com/sunlightlabs/django-simplepay/',
+    long_description=long_description,
+    platforms=["any"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+    ],
+)
